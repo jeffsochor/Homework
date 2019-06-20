1b5c047bd337bc3743de893ba9d87bf58b0c1a5a
Jeff Sochor <47911571+jeffsochor@users.noreply.github.com>
2019-06-20 18:36:03 -0500

    Done!

---
+++ Python-challenge/PyBank/main.py
@@ -0,0 +1,42 @@
+import csv
+import os
+
+csv_path = os.path.join("Resources", "budget_data.csv")
+
+revenue = []
+date = []
+rev_change = []
+
+with open(csv_path) as data:
+    reader = csv.reader(data)
+    header = next(reader)
+    for row in reader:
+        revenue.append(int(row[1]))
+        date.append(row[0])
+
+    for i in range(1,len(revenue)):
+        rev_change.append(revenue[i] - revenue[i-1])
+        avg_rev_change = sum(rev_change)/len(rev_change)
+        max_rev_change = max(rev_change)
+        min_rev_change = min(rev_change)
+        max_rev_change_date = str(date[rev_change.index(max(rev_change))])
+        min_rev_change_date = str(date[rev_change.index(min(rev_change))])
+
+print("Financial Analysis")
+print("-----------------------------------")
+print("Total Months:", len(date))
+print("Total: $", sum(revenue))
+print("Avereage Change: $",round(avg_rev_change))
+print("Greatest Increase in Profits", max_rev_change_date,"($",max_rev_change,")")
+print("Greatest Decrease in Profits:", min_rev_change_date,"($",min_rev_change,")")
+
+
+saveFile = open("FinancialAnalysis.txt", 'w')
+saveFile.write("Financial Analysis" + "\n")
+saveFile.write("-----------------------------------\n")
+saveFile.write("Total Months:" + str(len(date))+"\n")
+saveFile.write("Total: $" + str(sum(revenue))+"\n")
+saveFile.write("Avereage Change: $" + str(round(avg_rev_change))+"\n")
+saveFile.write("Greatest Increase in Profits" + str(max_rev_change_date) + "($" + str(max_rev_change) + ")" +"\n")
+saveFile.write("Greatest Decrease in Profits:" + str(min_rev_change_date)+ "($" + str(min_rev_change) + ")")
+saveFile.close()
---
+++ Python-challenge/PyPoll/main.py
@@ -0,0 +1,51 @@
+import os
+import csv
+
+csv_path = os.path.join("Resources", "election_data.csv")
+
+total_votes = 0
+candidate = ""
+candidate_votes = {}
+candidate_percentages ={}
+winner_votes = 0
+winner = ""
+
+with open(csv_path) as data:
+    reader = csv.reader(data)
+    header = next(reader)
+
+    for row in reader:
+        total_votes = total_votes + 1
+        candidate = row[2]
+        if candidate in candidate_votes:
+            candidate_votes[candidate] = candidate_votes[candidate] + 1
+        else:
+            candidate_votes[candidate] = 1
+
+
+for person, vote_count in candidate_votes.items():
+    candidate_percentages[person] = '{0:.0%}'.format(vote_count / total_votes)
+    if vote_count > winner_votes:
+        winner_votes = vote_count
+        winner = person
+
+
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------")
+for person, vote_count in candidate_votes.items():
+    print(f"{person}: {candidate_percentages[person]} ({vote_count})")
+print("-------------------------")
+print(f"Winner: {winner}")
+print("-------------------------")
+
+saveFile = open("ElectionResults.txt", 'w')
+saveFile.write("Election Results" + "\n")
+saveFile.write("-----------------------------------" + "\n")
+saveFile.write(f"Total Votes: {total_votes}" + "\n")
+saveFile.write("-------------------------" + "\n")
+saveFile.write("-------------------------" + "\n")
+saveFile.write(f"Winner: {winner}" + "\n")
+saveFile.write("-------------------------")
+saveFile.close()
